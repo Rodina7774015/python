d44207b6b32d86f12d9d6bb7f737ab8d9657dfae
lilo5310 <rodina8965777@yandex.ru>
2025-06-17 22:18:18 +0300

    Проверка на конец списка

--- gui.py
+++ gui.py
@@ -1,27 +1,27 @@
-user_name=input("Ввведите ваше имя")
 gmail_logins = [
     "techlover42@gmail.com",
     "sunny.daydreamer@gmail.com",
-    "quantum.leap@gmail.com",
-    "starry.night@gmail.com",
-    "bookworm.reader@gmail.com",
-    "neon.pulse@gmail.com",
-    "mystic.whisper@gmail.com",
-    "urban.explorer@gmail.com",
-    "luna.tides@gmail.com",
-    "retro.gamer@gmail.com"
 ]
-while not user_name.isalpha():
-    print("Вы ввели не имя ")
-    user_name=input("ВВведите ваше имя")
-user_login=input("Введите ваш логин")
-while "@gmail.com" not in user_login:
-    print("Логин не соответвует ")
-    user_login=input("Введите ваш логин")
-if user_login not in  gmail_logins:
-    print("В доступе отказано ")
-full_data=f"{user_name} {user_login}"
-print(full_data)
+flag=1
+while(flag):
+    user_name=input("Ввведите ваше имя :")
+    while not user_name.isalpha():
+        print("Вы ввели не имя ")
+        user_name=input("ВВведите ваше имя:")
+    user_login=input("Введите ваш логин:")
+    while "@gmail.com" not in user_login:
+        print("Логин не соответвует стандарту '@gmail.com' ")
+        user_login=input("Введите ваш логин:")
+    if not gmail_logins:
+        print("Все пользователи зашли")
+        break
+    if user_login not in gmail_logins:
+        print("В доступе отказано. ")
+    else:
+        gmail_logins.remove(user_login)
+        print("Есть доступ к программе. ")
+        full_data=f"{user_name} {user_login}"
+        print(full_data)
 
 
 
