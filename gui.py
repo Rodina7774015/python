dd2f20324a285c33153f5e25206df7bce2547d1c
lilo5310 <rodina8965777@yandex.ru>
2025-06-17 22:43:58 +0300

    Добавлены комментарии

--- gui.py
+++ gui.py
@@ -1,26 +1,27 @@
+#Список доступных gmail
 gmail_logins = [
     "techlover42@gmail.com",
     "sunny.daydreamer@gmail.com",
 ]
-flag=1
+flag=1 #На будующие так можно выйти из цикла при помощи этого флага  
 while(flag):
     user_name=input("Ввведите ваше имя :")
-    while not user_name.isalpha():
+    while not user_name.isalpha():#Проверка на то что введено имя
         print("Вы ввели не имя ")
-        user_name=input("ВВведите ваше имя:")
+        user_name=input("Введите ваше имя:")
     user_login=input("Введите ваш логин:")
-    while "@gmail.com" not in user_login:
+    while "@gmail.com" not in user_login:#Проверка на корректность 
         print("Логин не соответвует стандарту '@gmail.com' ")
         user_login=input("Введите ваш логин:")
-    if not gmail_logins:
-        print("Все пользователи зашли")
+    if not gmail_logins:#Проверка на пусоту списка 
+        print("Все пользователи авторизовались")
         break
     if user_login not in gmail_logins:
         print("В доступе отказано. ")
     else:
-        gmail_logins.remove(user_login)
+        gmail_logins.remove(user_login)#Удаление из списка 
         print("Есть доступ к программе. ")
-        full_data=f"{user_name} {user_login}"
+        full_data=f"{user_name} {user_login}"#Обеденение в одной переменной f-format
         print(full_data)
 
 
